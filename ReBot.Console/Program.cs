74200fe7789c3510f3f58590d685165be1ca1175
Brady Kelly <brady@bradykelly.net>
2021-12-18 12:40:36 +0200

    Add project files.

---
+++ ReBot.Console/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Remora.Discord.Gateway;
+using Remora.Discord.Gateway.Extensions;
+using Remora.Discord.Gateway.Results;
+using Remora.Results;
+
+var cancellationSource = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    eventArgs.Cancel = true;
+    cancellationSource.Cancel();
+};
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>()
+    .Build();
+
+var token = config["Bot:Token"];
+
+var services = new ServiceCollection()
+    .AddLogging(cfg => cfg.AddConsole())
+    .AddDiscordGateway(_ => config["Bot:Token"])
+    .BuildServiceProvider();
+
+var gatewayClient = services.GetRequiredService<DiscordGatewayClient>();
+var runResult = await gatewayClient.RunAsync(cancellationSource.Token);
+
+var log = services.GetRequiredService<ILogger<Program>>();
+
+if (!runResult.IsSuccess)
+{
+    switch (runResult.Error)
+    {
+        case ExceptionError exe:
+            {
+                log.LogError(exe.Exception, "Exception during gateway connection: {ExceptionMessage}", exe.Message);
+                break;
+            }
+        case GatewayWebSocketError:
+        case GatewayDiscordError:
+            {
+                log.LogError("Gateway error: {Message}", runResult.Error.Message);
+                break;
+            }
+        default:
+            {
+                log.LogError("Unknown error: {Message}", runResult.Error.Message);
+                break;
+            }
+    }
+}
+
+
---
+++ ReBot.Service/Program.cs
@@ -0,0 +1,10 @@
+using ReBot.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
